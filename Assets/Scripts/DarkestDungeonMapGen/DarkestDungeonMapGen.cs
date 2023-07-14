e2ce273304fd58d4b4a4e8198b282f47f5952859
toon-leader-bacon <bacon247@gmail.com>
2023-07-14 07:55:29 -0400

    Added ability to draw lines of rooms

--- Assets/Scripts/DarkestDungeonMapGen/DarkestDungeonMapGen.cs
+++ Assets/Scripts/DarkestDungeonMapGen/DarkestDungeonMapGen.cs
@@ -14,20 +14,21 @@ public class DarkestDungeonMapGen : MonoBehaviour
   {
     tilemap.SetTile(new Vector3Int(0, 0, 0), roomTile);
 
-    buildLine(new Vector2Int(3, 0), new Vector2Int(8, 1));
-    buildLine(new Vector2Int(3, 0), new Vector2Int(6, 8));
-    // Build the rooms 2nd so they override the halls
-    buildRoom(new Vector2Int(3, 0));
-    buildRoom(new Vector2Int(8, 1));
-    buildRoom(new Vector2Int(6, 8));
+    lineOfRooms(firstRoomTL: new Vector2Int(0, 5), directionBetweenRooms: new Vector2Int(5, 0), numberOfRooms: 4);
+
+
+    // buildLine(new Vector2Int(3, 0), new Vector2Int(8, 1));
+    // buildLine(new Vector2Int(3, 0), new Vector2Int(6, 8));
+    // // Build the rooms 2nd so they override the halls
+    // buildRoom(new Vector2Int(3, 0));
+    // buildRoom(new Vector2Int(8, 1));
+    // buildRoom(new Vector2Int(6, 8));
 
   }
 
-  void buildRoom(Vector2Int topLeftCorner)
+  void buildRoom(Vector2Int topLeftCorner, int roomSize = 2)
   {
-    // All rooms are 2x2
-    square(roomTile, 2, topLeftCorner);
-
+    square(roomTile, roomSize, topLeftCorner);
   }
 
   void buildLine(Vector2Int start, Vector2Int end)
@@ -67,5 +68,46 @@ public class DarkestDungeonMapGen : MonoBehaviour
       tilemap.SetTile(new Vector3Int(brcX, tlcY + delta), t);
     }
 
+  }
+
+  void lineOfRooms(Vector2Int firstRoomTL, Vector2Int directionBetweenRooms, int numberOfRooms, int roomSize = 2)
+  {
+    // Vector2Int firstRoomTL = new Vector2Int(5, 0);
+    // Vector2Int directionBetweenRooms = new Vector2Int(5, 0);
+    // int numberOfRooms = 4;
+    // int roomSize = 2;
+
+    // Draw the first room
+    buildRoom(firstRoomTL, roomSize);
+
+    // Start the loop from index 1 b/c we've already drawn the first room
+    for (int i = 1; i < numberOfRooms; i++)
+    {
+      // Draw the hall from the prev room to the cur room
+      // This avoids an off by one issue where we draw a final hallway to nowhere
+      Vector2Int currRoomTL = firstRoomTL + (i * directionBetweenRooms);
+      Vector2Int prevRoomTl = currRoomTL - directionBetweenRooms;
+      buildLine(prevRoomTl, currRoomTL);
+
+      // Build the current room
+      buildRoom(currRoomTL, roomSize);
+    }
+  }
+
+  void boxOfRooms()
+  {
+    int tlX = 5;
+    int tlY = 0;
+
+    int roomsInXDir = 2;
+    int roomsInYDir = 2;
+
+    int hallDistance = 5;
+    int roomSize = 2;
+
+    for (int roomX = 0; roomX < roomsInXDir; roomX++)
+    {
+
+    }
   }
 }
