6236d8c0a5e9e0652aa4a86732a1f762f729964d
toon-leader-bacon <bacon247@gmail.com>
2023-10-15 13:22:14 -0400

    WIP

--- Assets/Scripts/NocabmonMaps/NocabmonMapContainer.cs
+++ Assets/Scripts/NocabmonMaps/NocabmonMapContainer.cs
@@ -1,58 +1,55 @@
 using System.Collections.Generic;
 
-public class NocabmonMapContainer : Dictionary<HashableCoord, NocabTile>
+public class NocabmonMapContainer<TValue> : DictionaryCoords<TValue>
 {
-    public static readonly HashableCoord NORTH = new HashableCoord(0, 1);
-    public static readonly HashableCoord SOUTH = new HashableCoord(0, -1);
-    public static readonly HashableCoord EAST = new HashableCoord(1, 0);
-    public static readonly HashableCoord WEST = new HashableCoord(-1, 0);
+    public static readonly HashablePointInt NORTH = new(0, 1);
+    public static readonly HashablePointInt SOUTH = new(0, -1);
+    public static readonly HashablePointInt EAST = new(1, 0);
+    public static readonly HashablePointInt WEST = new(-1, 0);
 
-    public static readonly HashSet<HashableCoord> CARDINAL_DIRECTIONS = new HashSet<HashableCoord>
-    {
-        NORTH,
-        SOUTH,
-        EAST,
-        WEST
-    };
+    public static readonly HashSet<HashablePointInt> CARDINAL_DIRECTIONS =
+        new HashSet<HashablePointInt> { NORTH, SOUTH, EAST, WEST };
 
-    public static readonly HashableCoord NORTH_EAST = new HashableCoord(1, 1);
-    public static readonly HashableCoord SOUTH_EAST = new HashableCoord(1, -1);
-    public static readonly HashableCoord SOUTH_WEST = new HashableCoord(1, -1);
-    public static readonly HashableCoord NORTH_WEST = new HashableCoord(-1, 1);
-    public static readonly HashSet<HashableCoord> ORDINAL_DIRECTIONS = new HashSet<HashableCoord>
-    {
-        NORTH,
-        NORTH_EAST,
-        EAST,
-        SOUTH_EAST,
-        SOUTH,
-        SOUTH_WEST,
-        WEST,
-        NORTH_WEST
-    };
+    public static readonly HashablePointInt NORTH_EAST = new(1, 1);
+    public static readonly HashablePointInt SOUTH_EAST = new(1, -1);
+    public static readonly HashablePointInt SOUTH_WEST = new(1, -1);
+    public static readonly HashablePointInt NORTH_WEST = new(-1, 1);
+    public static readonly HashSet<HashablePointInt> ORDINAL_DIRECTIONS =
+        new HashSet<HashablePointInt>
+        {
+            NORTH,
+            NORTH_EAST,
+            EAST,
+            SOUTH_EAST,
+            SOUTH,
+            SOUTH_WEST,
+            WEST,
+            NORTH_WEST
+        };
 
-    public HashSet<NocabTile> getCardinalNeighbors(HashableCoord targetCoord)
+    public HashSet<TValue> getCardinalNeighbors(HashablePointInt targetCoord)
     {
         return getNeighbors_protected(targetCoord, CARDINAL_DIRECTIONS);
     }
 
-    public HashSet<NocabTile> getOrdinalNeighbors(HashableCoord targetCoord)
+    public HashSet<TValue> getOrdinalNeighbors(HashablePointInt targetCoord)
     {
         return getNeighbors_protected(targetCoord, ORDINAL_DIRECTIONS);
     }
 
-    protected HashSet<NocabTile> getNeighbors_protected(
-        HashableCoord targetCoord,
-        HashSet<HashableCoord> neighborDirections
+    protected HashSet<TValue> getNeighbors_protected(
+        HashablePointInt targetCoord,
+        HashSet<HashablePointInt> neighborDirections
     )
     {
-        HashSet<NocabTile> result = new HashSet<NocabTile>(4);
-        foreach (HashableCoord neighborDirection in neighborDirections)
+        HashSet<TValue> result = new HashSet<TValue>(4);
+        foreach (HashablePointInt neighborDirection in neighborDirections)
         {
-            HashableCoord neighborCoord = new HashableCoord(
-                targetCoord.coord.x + neighborDirection.coord.x, //
-                targetCoord.coord.y + neighborDirection.coord.y
-            );
+            HashablePointInt neighborCoord =
+                new(
+                    targetCoord.x + neighborDirection.x, //
+                    targetCoord.y + neighborDirection.y
+                );
             if (ContainsKey(neighborCoord))
             {
                 result.Add(this[neighborCoord]);
